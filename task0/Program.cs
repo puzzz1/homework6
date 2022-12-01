ec71afa625d0533eb6631a88fa7f8a59078d2a4d
puzz <taste@inbox.ru>
2022-12-01 19:57:50 +0300

    Make homework..

---
+++ task0/Program.cs
@@ -0,0 +1,49 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел.
+ Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+int[] UserArray(int length, int start, int end)
+{
+    Console.WriteLine(" Введите количество чисел");
+    length = int.Parse(Console.ReadLine());
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($" Введите число под индесом {i}");
+        array[i] = int.Parse(Console.ReadLine());
+    }
+    return array;
+
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.Write("]");
+}
+
+int PositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+
+    }
+    return count;
+}
+int[] lastarray = UserArray(5, -10, 10);
+PrintArray(lastarray);
+int result = PositiveNumbers(lastarray);
+Console.WriteLine($" Положительных моментов {result} штук ");
---
+++ task1/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Задача 43: Напишите программу, которая найдёт 
+точку пересечения двух прямых, заданных уравнениями 
+y = k1 * x + b1, y = k2 * x + b2;
+ значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+
+int getNumberFromUser(string message)
+{
+    int result = 0;
+    Console.WriteLine(message);
+    result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+double FindPoint (double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1)/ (k1-k2);
+    double y = k1 * x +b1;
+    Console.WriteLine($" Точка пересечения - {x} , {y}");
+    return x;
+
+}
+int b1 = getNumberFromUser(" Введите координаты В1");
+int k1 = getNumberFromUser(" Введите координаты K1");
+int b2 = getNumberFromUser(" Введите координаты В2");
+int k2 = getNumberFromUser(" Введите координаты K2");
+
+double final = FindPoint (b1, k1, b2, k2);
